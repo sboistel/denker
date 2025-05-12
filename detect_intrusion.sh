de1016ce6dc2b59334fac9c60767be01921205e1
Samuel Boistel <boistelsam@gmail.com>
2025-05-12 13:15:49 +0200

    fix: clearing paths

--- detect_intrusion.sh
+++ detect_intrusion.sh
@@ -3,12 +3,12 @@
 # VARS
 time=$(date +%Y-%m-%d)
 pub_ip=$(curl -s --max-time 2 ifconfig.me || echo "Uknown IP")
-img_path="/opt/denker/capture"
+img_path="$(pwd)/capture"
 
 # Check if Docker image exists
-docker_sender_exists=$(docker image ls | grep "docker_sender")
-if [ ! -n "$docker_sender_exists" ]; then
-  docker build -t denker /opt/denker/
+denker_exists=$(docker image ls | grep "denker")
+if [ ! -n "$denker_exists" ]; then
+  docker build -t denker $(pwd)
 fi
 
 # Watch for authentication failures
@@ -19,8 +19,8 @@ journalctl -f | while read -r line; do
 
     docker run --rm \
       -v "$img_path:/app/capture" \
-      docker_sender "$time Authentication failure from $pub_ip"
+      denker "$time Authentication failure from $pub_ip"
 
-    mv ${img_path}/${time}.jpg /opt/denker/capture/archive/
+    mv ${img_path}/${time}.jpg ./capture/archive/
   fi
 done
